d19190461281b65f462fe22b9ac9aa465e2d1324
Aaron Camm <40619424+AaronCamm@users.noreply.github.com>
2022-09-08 22:24:57 +1000

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,10 @@
 export const firebaseConfig = {
+  apiKey: "AIzaSyCf9orBfM18CrfDi_Ysn-NLBAmeFvJN9kA",
+  authDomain: "bendigosupport-42bbd.firebaseapp.com",
+  databaseURL: "https://bendigosupport-42bbd-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "bendigosupport-42bbd",
+  storageBucket: "bendigosupport-42bbd.appspot.com",
+  messagingSenderId: "521790147241",
+  appId: "1:521790147241:web:c36264051db11557b7f7b8",
+  measurementId: "G-MR1N3NBESX"
 };
